a4325dccd89077d1b522a0d5ac3404722ed59009
ogoss <343938328@qq.com>
2016-11-17 00:05:51 +0800

    添加播放动画回调函数

--- app/scripts/frame-player.js
+++ app/scripts/frame-player.js
@@ -18,13 +18,14 @@
 
   var framePlayer = {};
 
-  var frameList = {};
+
   var config = {
     nodeName: '#canvas',
     nodeClass: '',
     width: 750,
     height: 1207
   };
+  var frameList = {};
   var canvas;
   var context;
 
@@ -36,27 +37,10 @@
   var interval = 0;
   var looping = false;
   var loopObj = {};
-  var loopFunc;
-  var isFunc = false;
+  var onLoopStart = {};
+  var onLoopProcess = {};
+  var onLoopEnd = {};
 
-  function init(param) {
-    clone(config, param);
-
-    // 获取canvas外层DOM
-    var nodeName = document.querySelector(config.nodeName);
-    if (!nodeName) {
-      nodeName = document.createElement('div');
-      document.body.appendChild(nodeName);
-    }
-    nodeName.className += config.nodeClass;
-
-    // 添加canvas
-    canvas = document.createElement('canvas');
-    canvas.width = config.width;
-    canvas.height = config.height;
-    context = canvas.getContext('2d');
-    nodeName.appendChild(canvas);
-  }
 
   /**
    * 属性赋值
@@ -74,22 +58,33 @@
   }
 
   function startLoop() {
-    isFunc = isFunction(loopFunc);
+    onLoopStart.type = isFunction(onLoopStart.func);
+    onLoopProcess.type = isFunction(onLoopProcess.func);
+    onLoopEnd.type = isFunction(onLoopEnd.func);
+
     lastTime = Date.now();
     looping = true;
+
+    if (onLoopStart.type) {
+      onLoopStart.func();
+    }
+
     loop();
   }
 
   function loop() {
     if (!looping) {
+      if (onLoopEnd.type) {
+        onLoopEnd.func();
+      }
       return;
     }
 
     currentTime = Date.now();
     deltaTime = currentTime - lastTime;
 
-    if (isFunc) {
-      loopFunc();
+    if (onLoopProcess.type) {
+      onLoopProcess.func();
     }
 
     requestAnimationFrame(loop);
@@ -98,29 +93,6 @@
   function isFunction(functionToCheck) {
     var getType = {};
     return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-  }
-
-  /**
-   * 加载序列帧图片
-   * @param {Array} frames 序列帧数组
-   * @param {String} tag 序列帧索引
-   */
-  function loadFrame(frames, tag) {
-    frames = frames || [];
-    tag = tag || 'tag' + Math.round(new Date().getTime() / 1000);
-
-    frameList[tag] = {
-      status: 0,
-      res: []
-    };
-    
-    queue({
-      start: 0,
-      total: frames.length,
-      imageArr: [],
-      frames: frames,
-      tag: tag
-    });
   }
 
   function queue(params) {
@@ -159,12 +131,63 @@
     }, 2000);
   }
 
+
+  /**
+   * 初始化
+   * @param {Object} param 参数
+   */
+  function init(param) {
+    clone(config, param);
+
+    // 获取canvas外层DOM
+    var nodeName = document.querySelector(config.nodeName);
+    if (!nodeName) {
+      nodeName = document.createElement('div');
+      document.body.appendChild(nodeName);
+    }
+    nodeName.className += config.nodeClass;
+
+    // 添加canvas
+    canvas = document.createElement('canvas');
+    canvas.width = config.width;
+    canvas.height = config.height;
+    context = canvas.getContext('2d');
+    nodeName.appendChild(canvas);
+  }
+
+  /**
+   * 加载序列帧图片
+   * @param {Array} frames 序列帧数组
+   * @param {String} tag 序列帧索引
+   */
+  function loadFrame(frames, tag) {
+    frames = frames || [];
+    tag = tag || 'tag' + Math.round(new Date().getTime() / 1000);
+
+    frameList[tag] = {
+      status: 0,
+      res: []
+    };
+
+    queue({
+      start: 0,
+      total: frames.length,
+      imageArr: [],
+      frames: frames,
+      tag: tag
+    });
+
+    // TODO: 获取加载完成后状态
+  }
+
   /**
    * 播放序列帧
-   * @param  {String} tag 序列帧索引
-   * @param  {Number} duration 持续时间，单位ms
+   * @param {String} tag 序列帧索引
+   * @param {Number} duration 持续时间，单位ms
+   * @param {Function} onPlayStart 开始播放回调函数
+   * @param {Function} onPlayEnd 结束播放回调函数
    */
-  function play(tag, duration) {
+  function play(tag, duration, onPlayStart, onPlayEnd) {
     if (frameList[tag].status === 100) {
       start = 0;
       total = frameList[tag].res.length;
@@ -172,9 +195,11 @@
       loopObj = {
         tag: tag
       };
-      loopFunc = function func() {
+      onLoopStart.func = onPlayStart;
+      onLoopEnd.func = onPlayEnd;
+      onLoopProcess.func = function() {
         if ((deltaTime >= interval) && (start < total)) {
-          console.log(frameList[loopObj.tag].res[start]);
+          // console.log(frameList[loopObj.tag].res[start]);
           context.drawImage(frameList[loopObj.tag].res[start], 0, 0, config.width, config.height);
 
           start++;
