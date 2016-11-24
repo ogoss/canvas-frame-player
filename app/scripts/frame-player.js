b290529e90c2aa8e80c1c92c31c3b81a92886ddc
ogoss <343938328@qq.com>
2016-11-24 15:46:07 +0800

    添加功能：动画播放次数

--- app/scripts/frame-player.js
+++ app/scripts/frame-player.js
@@ -34,6 +34,7 @@
   var start = 0;
   var total = 0;
   var interval = 0;
+  var playtimes = 0;
   var looping = false;
   var loopObj = {};
   var onLoopStart = {};
@@ -78,7 +79,7 @@
    * 循环主函数
    */
   function loop() {
-    if (!looping) {
+    if (!looping || playtimes === 0) {
       (onLoopEnd.type && start === total) ? onLoopEnd.func(): '';
 
       return;
@@ -193,13 +194,15 @@
    * 播放序列帧
    * @param {String} tag 序列帧索引
    * @param {Number} duration 持续时间，单位ms
+   * @param {Number} times 动画播放次数 [times<0:无限次, times=0:不播放, times>0:播放times次]
    * @param {Function} onPlayStart 开始播放回调函数
    * @param {Function} onPlayEnd 结束播放回调函数
    */
-  function play(tag, duration, onPlayStart, onPlayEnd) {
+  function play(tag, duration, times, onPlayStart, onPlayEnd) {
     if (frameList[tag].status === 100) {
       start = 0;
       total = frameList[tag].res.length;
+      playtimes = times || 0;
       interval = duration / total;
       loopObj = {
         tag: tag
@@ -208,13 +211,16 @@
       onLoopEnd.func = onPlayEnd;
       onLoopProcess.func = function() {
         if ((deltaTime >= interval) && (start < total)) {
-          // console.log(frameList[loopObj.tag].res[start]);
           context.drawImage(frameList[loopObj.tag].res[start], 0, 0, config.width, config.height);
-
           start++;
           lastTime = currentTime;
         } else if (start >= total) {
-          looping = false;
+          if (playtimes != 0) {
+            start = 0;
+            playtimes--;
+          } else {
+            looping = false;
+          }
         }
       };
 
